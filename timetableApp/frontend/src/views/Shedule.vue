5d7af4c819dd3542f89a7552a0fdc1e8a5e9358b
Victor <evdokimovvik@gmail.com>
2019-12-06 14:57:48 +0500

    add algorithm

--- timetableApp/frontend/src/views/Shedule.vue
+++ timetableApp/frontend/src/views/Shedule.vue
@@ -2,13 +2,14 @@
   <v-container fluid>
     <v-row>
       <v-col class="shedule__params" cols="3" style="background: red">
-        <v-btn>Звонки</v-btn>
-        <v-btn>Классы</v-btn>
-        <v-btn>Предметы</v-btn>
-        <v-btn>Нагрузка класса по предметам</v-btn>
-        <v-btn>Учителя</v-btn>
-        <v-btn>Кабинеты</v-btn>
-        <v-btn>Настройки расписания</v-btn>
+        <div>
+          <router-link :to="{name: 'home'}">
+            Звонки
+          </router-link>
+        </div>
+        <router-link :to="{name: 'home'}">
+          Звонки
+        </router-link>
       </v-col>
       <v-col cols="9" style="background: blue">
         Параметры
